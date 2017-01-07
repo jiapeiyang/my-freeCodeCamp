7861ea2778dde25272e2f3ec6b63645677a109cf
yangjiapei <1098486124@qq.com>
2017-01-08 02:12:33 +0800

    Create Diff Two Arrays.js
    create

---
+++ Intermediate Algorithm Scripting/Diff Two Arrays.js
@@ -0,0 +1,14 @@
+/* Bonfire: Diff Two Arrays
+Difficulty: 2/5
+Compare two arrays and return a new array with any items not found in both of the original arrays.
+Remember to use RSAP if you get stuck. Try to pair program. Write your own code.
+Here are some helpful links:
+Comparison Operators
+String.slice()
+Array.filter()
+Array.indexOf()
+String.concat()
+Code by jiapeiyang
+1098486124@qq.com
+https://freecodecamp.cn/jiapeiyang
+*/
