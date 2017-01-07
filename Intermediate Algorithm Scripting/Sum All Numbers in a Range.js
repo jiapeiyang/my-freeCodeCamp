87a24ef6b3e2abc600617fd53182063ebf520f62
yangjiapei <1098486124@qq.com>
2017-01-08 02:06:52 +0800

    Update Sum All Numbers in a Range.js
    add two methods

--- Intermediate Algorithm Scripting/Sum All Numbers in a Range.js
+++ Intermediate Algorithm Scripting/Sum All Numbers in a Range.js
@@ -15,16 +15,41 @@ Code by jiapeiyang
 https://freecodecamp.cn/jiapeiyang
 */
 
-// the first method
+// first method
 function sumAll(arr) {
- var maxNum = Math.max.apply(null, arr);
- var minNum = Math.min.apply(null, arr);
+ var maxNum = Math.max.apply(null, arr);  // or Math.max.apply(Math, arr) or Math.max(...arr); ES6 展开运算符
+ var minNum = Math.min.apply(null, arr);  // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Operators/Spread_operator
  var sum = 0;
  for(var i= minNum; i<= maxNum; i++) {
    sum +=i;
-   console.log(sum);
  }
  return sum;
+}
+
+// second method
+function range (minNum, maxNum) {
+  var newArr = [];
+  for (var i=minNum; i<=maxNum; i++) {
+    newArr.push(i);
+  }
+  return newArr;
+}
+
+function sumAll(arr) {
+  var maxNum = Math.max.apply({}, arr);
+  var minNum = Math.min.apply({}, arr);
+  
+  var sum = range(minNum, maxNum).reduce(function (previous, current) {
+      return previous + current;
+  });
+  return sum;
+}
+
+// third method
+function sumAll(arr) {
+  var max = Math.max(arr[0],arr[1]);
+  var min = Math.min(arr[0], arr[1]);
+  return (max - min + 1) * (min + max) / 2;
 }
 
 sumAll([1, 4]);
