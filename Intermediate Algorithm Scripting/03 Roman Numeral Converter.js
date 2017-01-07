6346812616eb185495e0520c2ceadf902303b092
yangjiapei <1098486124@qq.com>
2017-01-08 03:26:53 +0800

    Create 03 Roman Numeral Converter.js
    create

---
+++ Intermediate Algorithm Scripting/03 Roman Numeral Converter.js
@@ -0,0 +1,12 @@
+/* Bonfire: Roman Numeral Converter
+Difficulty: 2/5
+Convert the given number into a roman numeral.
+All roman numerals answers should be provided in upper-case.
+Remember to use RSAP if you get stuck. Try to pair program. Write your own code.
+Here are some helpful links:
+Array.splice() Array.indexOf() Array.join()
+
+Code by jiapeiyang
+1098486124@qq.com
+http://www.freecodecamp.com/jiapeiyang
+*/
